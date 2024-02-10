e80801015f4123180e5c569de56f8b523203ea48
nico <nico.a.judge@gmail.com>
2024-02-10 07:45:20 -0500

    removed target folder from repo

--- target/debug/data_comparison_tool.d
+++
@@ -1,1 +0,0 @@
-/home/nico/solutions/data_comparison_tool/target/debug/data_comparison_tool: /home/nico/solutions/data_comparison_tool/src/data.rs /home/nico/solutions/data_comparison_tool/src/main.rs
--- target/debug/deps/pkg_config-a4294588759669c4.d
+++
@@ -1,7 +0,0 @@
-/home/nico/RustroverProjects/data_comparison_tool/target/debug/deps/libpkg_config-a4294588759669c4.rmeta: /home/nico/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pkg-config-0.3.29/src/lib.rs
-
-/home/nico/RustroverProjects/data_comparison_tool/target/debug/deps/libpkg_config-a4294588759669c4.rlib: /home/nico/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pkg-config-0.3.29/src/lib.rs
-
-/home/nico/RustroverProjects/data_comparison_tool/target/debug/deps/pkg_config-a4294588759669c4.d: /home/nico/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pkg-config-0.3.29/src/lib.rs
-
-/home/nico/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pkg-config-0.3.29/src/lib.rs:
